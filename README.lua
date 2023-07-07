42541cb515f27a77a5122571fb33bb55895aaf8c
$tIX <82320835+Icehuy@users.noreply.github.com>
2023-07-07 12:40:09 +0300

    Update README.lua

--- README.lua
+++ README.lua
@@ -2976,7 +2976,7 @@ function Library:CreateWindow(...)
         BackgroundColor3 = Library.MainColor;
         BorderColor3 = Library.AccentColor;
         BorderMode = Enum.BorderMode.Inset;
-        Position = UDim2.new(0, 1, 0, 1);
+        Position = UDim2.new(0, 1, 5, 1);
         Size = UDim2.new(1, -2, 1, -2);
         ZIndex = 1;
         Parent = Outer;
