cec79e174fa83a8f19712fb6848e978e4184f85b
$tIX <82320835+Icehuy@users.noreply.github.com>
2023-07-07 12:43:08 +0300

    Update README.lua

--- README.lua
+++ README.lua
@@ -2988,8 +2988,8 @@ function Library:CreateWindow(...)
     });
 
     local WindowLabel = Library:CreateLabel({
-        Position = UDim2.new(10, 1, 0, 0);
-        Size = UDim2.new(0, 0, 0, 25);
+        Position = UDim2.new(0, 1, 0, 0);
+        Size = UDim2.new(0, 0, 0, 50);
         Text = Config.Title or '';
         TextXAlignment = Enum.TextXAlignment.Left;
         ZIndex = 1;
